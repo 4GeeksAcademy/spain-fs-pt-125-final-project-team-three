ab9ccc9a20c3f5f3ba471085ad9a757e00de94cb
diegocalzadilla09 <diegocalzadilla09@gmail.com>
2026-02-20 20:26:44 +0000

    alpha

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -4,11 +4,13 @@ import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
 import MyMap from "../components/MyMap"
 import RestaurantsNearby from "../components/RestaurantsNearby"
+import { Home } from "./Home"
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
   return (
     <div>
+      <Home />
       <h1>Mapa</h1>
       <MyMap />
       <h1>Restaurantes cercanos</h1>
