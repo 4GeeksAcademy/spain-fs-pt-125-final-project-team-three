e048765cb347fc922282fe4dc91bf2e8a995ddda
JorgeS263 <jorgee.serraano@gmail.com>
2026-02-27 12:11:21 +0000

    Login&Registroactualizados

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Hecho con <i className="fa fa-heart text-danger" /> por Jorge,Diego y Gilbert - 2026
+			Hecho con <i className="fa fa-heart text-danger" /> por Jorge, Diego y Gilbert - 2026
 		</p>
 	</footer>
 );
